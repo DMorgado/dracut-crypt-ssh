24ad159d31ee3591f5fa10b791423e1f55cdfa49
David Morgado <david@out750.cm-sintra.local>
2017-09-20 16:36:38 +0100

    Flush network interfaces IP configuration on dropbear stop.

--- modules/60crypt-ssh/dropbear-stop.sh
+++ modules/60crypt-ssh/dropbear-stop.sh
@@ -6,3 +6,9 @@ kill -STOP ${main_pid} 2>/dev/null
 pkill -P ${main_pid}
 kill ${main_pid} 2>/dev/null
 kill -CONT ${main_pid} 2>/dev/null
+
+# Flush all IP Configuration on all Interfaces 
+# so not to interfere with NetworkManager
+for i in $(ls /sys/class/net/) ; do
+    /usr/sbin/ip addr flush $i &
+done
